2fc39f927226cbb1480887f0fe783ba8aa25fe3d
Till Sanders <mail@till-sanders.de>
2024-10-17 13:43:38 +0200

    test: fix demo setup
    Signed-off-by: Till Sanders <mail@till-sanders.de>

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import tseslint from "typescript-eslint";
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.browser } },
-  { ignores: ["dist/*", "node_modules/*"] },
+  { ignores: ["dist/*", "node_modules/*", "demo/dist/*"] },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
